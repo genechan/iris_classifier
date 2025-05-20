4ef1fda5de4393df384b25e07ac06d6e7b17599b
genechan <genechan@users.noreply.github.com>
2025-05-20 15:51:29 -0400

    update for render.com deploy

--- README.md
+++ README.md
@@ -31,3 +31,8 @@ curl -X POST -H "Content-Type: application/json" -d '{"sepal_length": 5.1, "sepa
 ```
 {"species":"setosa"}
 ```
+
+### curl on render.com
+
+curl -X POST -H "Content-Type: application/json" -d '{"sepal_length": 5.1, "sepal_width": 3.5, "petal_length": 1.4, "petal_width": 0.2}'
+https://iris-classifier-gaqi.onrender.com
--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -24,5 +25,10 @@ def predict():
     prediction = model.predict(features)[0]
     return jsonify({'species': species[prediction]})
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Iris Classifier API is running!"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
